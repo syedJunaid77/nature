a25d9fcd9bc9272266018c738827e47d980e2ab1
suhail <sayyedsuhail800900@gmail.com>
2024-05-29 10:27:11 +0530

    Added three pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
       backgroundImage: {
         "hero-pattern": "url('./../src/img/1-home.jpg')",
         "hero-pattern2": "url('./../src/img/7-home.jpg')",
+        "hero-pattern3": "url('./../src/img/1-about.jpg')",
+        "hero-pattern4": "url('./../src/img/1-services.jpg')",
+        "hero-pattern5": "url('./../src/img/2-services.jpg')",
+        "hero-pattern6": "url('./../src/img/1-contact.jpg')",
       },
     },
   },
